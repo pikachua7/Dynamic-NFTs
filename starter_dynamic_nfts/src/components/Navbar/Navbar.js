6e3b92b70c9c776b233dc9086a49a12faf0c817d
pikachua7 <atharvapaliwal7@gmail.com>
2022-03-08 22:39:40 +0530

    Navbar Added

--- starter_dynamic_nfts/src/components/Navbar/Navbar.js
+++ starter_dynamic_nfts/src/components/Navbar/Navbar.js
@@ -15,6 +15,42 @@ export default function Navbar(loading, currentNetwork, reset) {
     history.push('/');
   }
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <div className="container">
+        
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <Link className="nav-link text-primary-color" to="/">Dashboard</Link>
+            </li>
+            {walletAddress &&
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                <Link className="nav-link text-primary-color" to="/userRewards">My Rewards</Link>
+              </li>
+            }
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item d-flex align-items-center" data-toggle="collapse" data-target=".navbar-collapse.show">
+              
+              {walletAddress ? <a
+                target="_blank"
+                className="nav-link text-primary-color"
+                rel="noopener noreferrer"
+                href={"https://explorer-mumbai.maticvigil.com/address/" + walletAddress}>
+                {walletAddress.substring(0,5)}...{walletAddress.substring(37,42)}
+              </a> : <button className="btn secondary-bg-color" data-toggle="modal" data-target="#walletModal" disabled={loading}>Open Wallet</button>
+              }
+            </li>
+            {walletAddress && 
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                <button className="btn" onClick={() => handleLogout()}>Disconnect Wallet</button>
+              </li>
+            }
+          </ul>
+        </div>
+        
+      </div>
+      
+    </nav>
   )
 }
